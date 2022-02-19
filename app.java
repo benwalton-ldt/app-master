6db15231afe896b5df5ca91f2ce97de6f08f1df4
benwalton7 <benzwalton7@gmail.com>
2022-02-19 07:38:26 -0500

    New edit by benwalton version 4.0

--- app.java
+++ app.java
@@ -7,3 +7,10 @@ read username
 echo"plead enter your password"
 read -s password
 echo "welcome to the zeel application"
+
+#This an edit from bwalton7
+echo "If you are a Team-Champion, Please enter your name"
+read name
+echo "Thanks $name for being a champion"
+echo "You are very valuable"
+echo "From today `date` you will be a success"
